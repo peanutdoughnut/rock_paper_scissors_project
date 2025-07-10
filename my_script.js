9c3336d4d5e98a9ddd828c573df76036572f2f7d
peanutdoughnut <cjkwanthe@gmail.com>
2025-07-10 14:06:42 +0200

    Write get human choice function
    added prompt for human choice
    
    added loop to repeat five times

---
+++ my_script.js
@@ -0,0 +1,12 @@
+for(let i=0; i<5; i++){
+let userChoice = prompt("Enter rock, paper or scissors: ");
+
+userChoice = userChoice.toLowerCase(userChoice);
+
+console.log(userChoice);
+alert(userChoice);
+}
+
+function getHumanChoice(){
+    
+}
