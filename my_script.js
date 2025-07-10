e06d3b99b564cee7f3e90fc4ff8580e469f47a7d
peanutdoughnut <cjkwanthe@gmail.com>
2025-07-10 14:58:17 +0200

    Add playround function
    Write function for playing each round
    added playround()  to the loop section
     fixed if statement syntax

--- my_script.js
+++ my_script.js
@@ -1,28 +1,55 @@
-/*for(let i=0; i<5; i++){
-let userChoice = prompt("Enter rock, paper or scissors: ");
-
-userChoice = userChoice.toLowerCase(userChoice);
-
-console.log(userChoice);
-alert(userChoice);
-} */
-
-function getComputerChoice(){
+//computer choice function
+function getComputerChoice()
+{
     let num = Math.random();
     let computerChoice;
     if (num <= 0.3){ 
-         computerChoice = 'rock'
-        }
-        else if (num >0.3 && num <+ 0.6){
+      computerChoice = 'rock'
+    }
+    else if (num >0.3 && num <= 0.6){
             computerChoice = 'paper'
-        }
-            else if (num >0.6 && num <= 1){
+   }
+    else if (num >0.6 && num <= 1){
                 computerChoice = 'scissors'
-            }
+   }
     return computerChoice;
 }
+// play round function
+function playRound(humanSelection, computerSelection) {
+    
+    //humanSelection = userChoice ;
+
+    if (humanSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        (humanSelection === 'rock' && computerSelection === 'scissors') ||
+        (humanSelection === 'paper' && computerSelection === 'rock') ||
+        (humanSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        return "You win! " + humanSelection + " beats " + computerSelection;
+    } else {
+        return "You lose! " + computerSelection + " beats " + humanSelection;
+    }
+} 
 
 
+
+//loop section
+for(let i=0; i<5; i++){
+ let userChoice = prompt("Enter rock, paper or scissors: ");
+ userChoice = userChoice.toLowerCase();
 const computerSelection = getComputerChoice();
-console.log(computerSelection);
-alert(computerSelection);
+const humanSelection = userChoice;
+console.log(`computer:${computerSelection}`);
+ playRound(humanSelection, computerSelection);
+ 
+}
+
+//user choice function
+
+
+/*console.log(userChoice);
+alert(userChoice);*/
+
+
+
