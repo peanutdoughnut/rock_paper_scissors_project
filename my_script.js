f3ffb4f57e697d77643a664e83f7df153351bc79
peanutdoughnut <cjkwanthe@gmail.com>
2025-07-10 14:26:50 +0200

    Add getComputerChoice function
    Added function to generate getComputerChoice
    using Math.random()

--- my_script.js
+++ my_script.js
@@ -1,12 +1,28 @@
-for(let i=0; i<5; i++){
+/*for(let i=0; i<5; i++){
 let userChoice = prompt("Enter rock, paper or scissors: ");
 
 userChoice = userChoice.toLowerCase(userChoice);
 
 console.log(userChoice);
 alert(userChoice);
+} */
+
+function getComputerChoice(){
+    let num = Math.random();
+    let computerChoice;
+    if (num <= 0.3){ 
+         computerChoice = 'rock'
+        }
+        else if (num >0.3 && num <+ 0.6){
+            computerChoice = 'paper'
+        }
+            else if (num >0.6 && num <= 1){
+                computerChoice = 'scissors'
+            }
+    return computerChoice;
 }
 
-function getHumanChoice(){
-    
-}
+
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+alert(computerSelection);
